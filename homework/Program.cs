d3bb0070066618d095569f16f883136f868f7474
mejiyanan <68030238@kmitl.ac.th>
2025-06-11 15:37:49 +0700

    finish

---
+++ homework/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("กรุณากรอกอายุ: ");
+int age = int.Parse(Console.ReadLine());
+
+if (age >= 1 && age <= 12)
+{
+    Console.WriteLine("เด็ก");
+}
+else if (age >= 13 && age <= 19)
+{
+    Console.WriteLine("วัยรุ่น");
+}
+else if (age >= 20 && age <= 50)
+{
+    Console.WriteLine("วัยผู้ใหญ่");
+}
+else if (age >= 51)
+{
+    Console.WriteLine("วัยชรา");
+}
+else
+{
+    Console.WriteLine("กรุณากรอกอายุที่ถูกต้อง");
+}
